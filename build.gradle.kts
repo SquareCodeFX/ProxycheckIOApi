d7f7792a4b8c1ac792c3ce170663ac7d77cdb39d
square <admin@squarecode.de>
2025-03-23 12:57:27 +0100

    Set JVM target to 11 for Kotlin compilation
    Configured KotlinCompile tasks to target JVM 11 in the build script. This ensures compatibility and

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,3 +33,9 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
